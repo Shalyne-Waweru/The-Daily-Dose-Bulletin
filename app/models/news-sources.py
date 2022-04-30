f4fe1317661c656e646e412f8ea171d5ac73305b
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-30 22:27:53 +0300

    Create the News Sources Class

---
+++ app/models/news-sources.py
@@ -0,0 +1,11 @@
+class Sources:
+  '''
+  Sources class to define News Sources Objects
+  '''
+
+  def __init__(self,name,description,url,country):
+    self.name = name
+    self.description = description
+    self.url = url
+    self.country = country
+   
