bf1871bd43e95996800d62c04b551face5576147
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-05-02 01:41:35 +0300

    Make the technology category Api Call

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_sources,get_headlines
+from .request import get_sources,get_headlines,get_articles
 
 @app.route('/')
 def landing_page():
@@ -15,3 +15,15 @@ def landing_page():
   news_headlines = get_headlines()
 
   return render_template('index.html', news_sources = news_sources, news_headlines = news_headlines)
+
+@app.route('/top-headlines?category=technology')
+def tech_page():
+  '''
+  View tech_page function that returns the tech page and its data
+  '''
+
+  #Get the News Articles
+  news_articles = get_articles('technology')
+
+  return render_template('tech_page.html', news_articles = news_articles)
+
